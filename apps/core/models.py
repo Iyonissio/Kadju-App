2bbde2205652c68eda6d4d157409bf57bf6e7c4e
Iyonissio Daniel Sitoe <57901563+Iyonissio@users.noreply.github.com>
2021-11-15 09:00:50 +0200

    Project

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Destaque(models.Model):
     nome = models.CharField(max_length=100)
     image = models.ImageField(upload_to='uploads/', blank=True, null=True)
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-
 from apps.product.models import Product
-
 from .models import Destaque
 
 def frontpage(request):
@@ -9,8 +7,8 @@ def frontpage(request):
     return render(request, 'core/frontpage.html', {'newest_products': newest_products})
 
 def destaque(request):
-    novos_destaques = Destaque.objects.all()
-    return render(request, 'core/frontpage.html', {'novos_destaques':novos_destaques})
+    novos_destaques = Destaque.objects.all()[0:8]
+    return render(request, 'core/frontpage.html', {'novos_destaques': novos_destaques})
 
 def contact(request):
     return render(request, 'core/contact.html')
